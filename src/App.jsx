cee4bfe71e93356d9b968b1b240b49da9f2cb13b
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-10-18 21:11:25 +0700

    Set fill current for svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { ReactComponent as Logo } from './assets/icons/square.svg';
+import { ReactComponent as Logo } from './assets/icons/arrow-down-left.svg';
 
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
-      <Logo className="fill-green-500" />
+      <Logo className="fill-black-500" />
     </h1>
   );
 }
