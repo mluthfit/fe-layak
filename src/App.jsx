8a87b5041b0c0b9c8892cf0eb1ecd1599590bb26
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-10-25 10:34:12 +0700

    Merge sidebar and topbar to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Overview from "./pages/Overview";
 import UserCuti from "./pages/UserCuti";
 
 const App = () => {
+  const roleUser = "user";
+
   return (
     <Routes>
       <Route path="/" element={<div>Hello World</div>} />
-      <Route path="/dashboard" element={<Dashboard />}>
+      <Route path="/dashboard" element={<Dashboard role={roleUser} />}>
         <Route index element={<Overview />} />
         <Route path="cuti" element={<UserCuti />} />
       </Route>
--- src/layouts/Dashboard/index.jsx
+++ src/layouts/Dashboard/index.jsx
@@ -1,15 +1,95 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { Outlet } from "react-router-dom";
-import Sidebar from "../../components/Sidebar";
-import Topbar from "../../components/Topbar";
+import { Outlet, useLocation } from "react-router-dom";
+import { ReactComponent as OverviewIcon } from "../../assets/icons/square.svg";
+import { ReactComponent as AbsensiIcon } from "../../assets/icons/camera.svg";
+import { ReactComponent as CutiIcon } from "../../assets/icons/calendar-dates.svg";
+import { ReactComponent as ReimburseIcon } from "../../assets/icons/calculator.svg";
+import { ReactComponent as HideIcon } from "../../assets/icons/chevrons-left.svg";
+import { ReactComponent as LogoutIcon } from "../../assets/icons/logout.svg";
 import style from "./style.module.css";
 
-const Dashboard = () => {
+const Dashboard = ({ role }) => {
+  const location = useLocation();
+  const page = location.pathname.split("/")[2] ?? "overview";
+
+  const hideSidebar = () => {
+    const sidebarEl = document.querySelector("#sidebar");
+    sidebarEl.classList.toggle(`${style.hide}`);
+  };
+
+  (() => {
+    const root = document.querySelector(":root");
+    const rootCS = getComputedStyle(root);
+
+    let background = rootCS.getPropertyValue("--secondary-200");
+    let titleColor = rootCS.getPropertyValue("black");
+    let primaryColor = rootCS.getPropertyValue("--primary-400");
+
+    if (["admin", "super_admin"].includes(role)) {
+      background = rootCS.getPropertyValue("--primary-100");
+      titleColor = rootCS.getPropertyValue("--primary-400");
+      primaryColor = rootCS.getPropertyValue("--secondary-300");
+    }
+
+    root.style.setProperty("--background", background);
+    root.style.setProperty("--primary-color", primaryColor);
+    root.style.setProperty("--title-color", titleColor);
+  })();
+
   return (
     <div className={style.dashboard}>
-      <Sidebar />
+      <div id="sidebar" className={style.sidebar}>
+        <div className={style.container}>
+          <ul className={`${style.menu} ${style.topMenu}`}>
+            <li className={page === "overview" ? `${style.active}` : ""}>
+              <a href="/dashboard">
+                <OverviewIcon />
+                <span>Overview</span>
+              </a>
+            </li>
+            <li className={page === "absensi" ? `${style.active}` : ""}>
+              <a href="/dashboard/absensi">
+                <AbsensiIcon />
+                <span>Absensi</span>
+              </a>
+            </li>
+            <li className={page === "cuti" ? `${style.active}` : ""}>
+              <a href="/dashboard/cuti">
+                <CutiIcon />
+                <span>Cuti</span>
+              </a>
+            </li>
+            <li className={page === "reimbursement" ? `${style.active}` : ""}>
+              <a href="/dashboard/reimbursement">
+                <ReimburseIcon />
+                <span>Reimbursement</span>
+              </a>
+            </li>
+          </ul>
+          <ul className={style.menu}>
+            <li>
+              <a onClick={hideSidebar}>
+                <HideIcon />
+                <span>Sembunyikan</span>
+              </a>
+            </li>
+            <li>
+              <a href="/logout">
+                <LogoutIcon />
+                <span>Logout</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div id="mainbar" className={style.mainbar}>
-        <Topbar />
+        <div className={style.topbar}>
+          <h1>Dashboard</h1>
+          <a className={style.changeButton} href="/admin">
+            Admin Dashboard
+          </a>
+        </div>
         <div className={style.content}>
           <Outlet />
         </div>
