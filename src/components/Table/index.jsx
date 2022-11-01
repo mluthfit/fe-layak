c4f020cc2626dda517e2db5869aad18b7c1d6e2a
Muhammad Luthfi <mluthfitaufiq@gmail.com>
2022-11-02 06:23:19 +0700

    Integration create user

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,54 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { keyToCapitalize } from "../../scripts/string";
 import style from "./style.module.css";
 
-const Table = ({ rows, iconLabel, icons, href, isHistory }) => {
+const Table = ({ type, label, rows, icon, href }) => {
   return (
-    <div className={`${style.table} ${isHistory ? `${style.historyRow}` : ""}`}>
-      <div className={style.thead}>
-        <div className={`${style.tr} gray`}>
-          {Object.keys(rows[0]).map((key, index) => (
-            <div className={style.td} key={index}>
-              {keyToCapitalize(key, "_")}
+    <div
+      className={`${style.table} ${
+        type === "history" ? `${style.historyRow}` : ""
+      }`}
+    >
+      {rows.length <= 0 ? (
+        <span className={style.notFound}>Data tidak ditemukan</span>
+      ) : (
+        <>
+          <div className={style.thead}>
+            <div className={`${style.tr} gray`}>
+              {label.map((value, index) => (
+                <div className={style.td} key={index}>
+                  {value}
+                </div>
+              ))}
+              <div className={style.td}>{icon.label}</div>
             </div>
-          ))}
-          <div className={style.td}>{iconLabel}</div>
-        </div>
-      </div>
-      <div className={style.tbody}>
-        {!href ? (
-          <>
-            {rows.map((row, parentIdx) => (
-              <div key={parentIdx} className={style.tr}>
-                {Object.values(row).map((value, childIdx) => (
-                  <div className={style.td} key={`${parentIdx}${childIdx}`}>
-                    {value}
+          </div>
+          <div className={style.tbody}>
+            {!href ? (
+              <>
+                {rows.map((row, parentIdx) => (
+                  <div key={parentIdx} className={style.tr}>
+                    {Object.values(row).map((value, childIdx) => (
+                      <div className={style.td} key={`${parentIdx}${childIdx}`}>
+                        {value}
+                      </div>
+                    ))}
+                    <div className={style.td}>
+                      <div className={style.icons}>
+                        {icon.element[parentIdx]}
+                      </div>
+                    </div>
                   </div>
                 ))}
-                <div className={style.td}>
-                  <div className={style.icons}>{icons[parentIdx]}</div>
-                </div>
-              </div>
-            ))}
-          </>
-        ) : (
-          <>
-            {rows.map((row, parentIdx) => (
-              <Link to={href[parentIdx]} key={parentIdx} className={style.tr}>
-                {Object.values(row).map((value, childIdx) => (
-                  <div className={style.td} key={`${parentIdx}${childIdx}`}>
-                    {value}
-                  </div>
+              </>
+            ) : (
+              <>
+                {rows.map((row, parentIdx) => (
+                  <Link
+                    to={href[parentIdx]}
+                    key={parentIdx}
+                    className={style.tr}
+                  >
+                    {Object.values(row).map((value, childIdx) => (
+                      <div className={style.td} key={`${parentIdx}${childIdx}`}>
+                        {value}
+                      </div>
+                    ))}
+                    <div className={style.td}>
+                      <div className={style.icons}>
+                        {icon.element[parentIdx]}
+                      </div>
+                    </div>
+                  </Link>
                 ))}
-                <div className={style.td}>
-                  <div className={style.icons}>{icons[parentIdx]}</div>
-                </div>
-              </Link>
-            ))}
-          </>
-        )}
-      </div>
+              </>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
